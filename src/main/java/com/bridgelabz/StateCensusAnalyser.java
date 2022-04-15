ead64c9dc01461df1721e8513fcbaa7003afd536
sspgithub123 <99379178+sspgithub123@users.noreply.github.com>
2022-04-15 11:59:16 +0530

    Update StateCensusAnalyser.java

--- src/main/java/com/bridgelabz/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -1,5 +1,35 @@
 package com.bridgelabz;
 
+/**********************************************************
+ * Program on User Registration Using Regex
+ *
+ * @author : Shubham Pawar
+ * @since : 14/04/2022
+ **********************************************************/
+
+/**
+ * UC1 :-   Ability for the analyser to load the Indian States Census Information from a csv file
+ *          - Create a StateCensusAnalyser Class to load  the State Census CSV Data
+ *          - Create CSVStateCensus Class to load the CSV Data
+ *          - Use Iterator to load the data
+ *          - Check with StateCensusAnalyser to ensure no of record matches.
+ 
+ * TC1.1 :- Given the States Census CSV file, Check to ensure the Number of Record matches
+ *          - This is a Happy Test Case where the records are checked
+ 
+ * TC1.2 :- Given the State Census CSV File if incorrect Returns a custom Exception
+ *          - This is a Sad Test Case to verify if the exception is raised.
+ 
+ * TC1.3 :- Given the State Census  CSV File when correct but type incorrect Returns a custom Exception
+ *          - This is a Sad Test Case to verify if the type is incorrect then exception is raised.
+ *
+ * TC1.4 :- Given the State Census CSV File when correct but delimiter incorrect Returns a custom Exception
+ *          - This is a Sad Test Case to verify if the file delimiter is incorrect then exception is raised.
+ *
+ * TC1.5 :- Given the State Census CSV File when correct but csv header incorrect Returns a custom Exception
+ *          - This is a Sad Test Case to verify if the header is incorrect then exception is raised.
+ */
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
